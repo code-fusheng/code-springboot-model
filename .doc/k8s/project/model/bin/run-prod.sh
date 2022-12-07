fe466a8103cee17df4c6a80b89b951ee8b5e5b21
code-fusheng <2561035977@qq.com>
2022-12-07 23:34:35 +0800

    file : 配置管理 --- 新增 K8S 工程项目部署配置文件以及脚本

---
+++ .doc/k8s/project/model/bin/run-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker login -u admin -p Harbor12345 42.192.222.62:9191
+
+docker tag model:latest 42.192.222.62:9191/model/model:latest
+docker push 42.192.222.62:9191/model/model:latest
+
+cd /root/Document/k8s/app/model
+
+#kubectl delete -f yaml.d/model-prod.yaml
+#kubectl apply -f yaml.d/model-prod.yaml
+
+kubectl rollout restart -n prod deployment model-prod
